c0ede0c40a326ee9d02fc5f7b896a15fd7eced28
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2021-10-10 14:02:58 -0300

    comeco

---
+++ js/modules/galeria.js
@@ -0,0 +1,25 @@
+export default function galeria(){
+    // galeria
+
+const fotoGrande = document.querySelector(".product__single-wrapper-gallery-picture img");
+
+const fotoThumbs = document.querySelectorAll(".product__single-wrapper-gallery-thumbs img")
+
+
+
+
+fotoThumbs.forEach((img, index)=>{
+    img.addEventListener("click", trocaFoto); 
+})
+
+
+function trocaFoto(evento){
+  fotoThumbs.forEach((item)=>{
+        item.classList.remove("ativo")
+    });
+    const dataFoto = evento.target.dataset.foto;
+    this.classList.add("ativo");
+    fotoGrande.src= dataFoto;
+}
+
+}
